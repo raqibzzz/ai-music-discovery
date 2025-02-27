6e90e75cfbb015cab0a89132abebe53917e38e0a
raqibzzz <raqibmuktadir2@gmail.com>
2025-02-27 14:27:05 -0500

    fixed image domains

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   /* config options here */
   webpack(config) {
     return config;
-  }
+  },
+  images: {
+    domains: ['i.scdn.co'],
+  },
 };
 
 module.exports = nextConfig; 
